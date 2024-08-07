901db93ac8da5bbb1e39f22a4eea339d8bdeed5e
Tejasmalokar2002 <113241900+Tejasmalokar2002@users.noreply.github.com>
2024-08-07 06:19:28 -0700

    Add files via upload
    HIbernate Database Connection and Data Fetch Successful

---
+++ FirstHibernate/src/main/java/org/example/Employee.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Employee {
+    @Id
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
